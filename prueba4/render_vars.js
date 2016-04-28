a4c72b3b5f41da2778994a89e3621032d9e3ffa3
Rubén Soler <r.solerginer@gmail.com>
2016-04-28 22:53:45 +0200

    motor de vistas 1.0

--- prueba4/render_vars.js
+++ prueba4/render_vars.js
@@ -4,13 +4,18 @@ var fs = require("fs");
 
 http.createServer(function (req, res) {
     var html = fs.readFile("./index.html", function (err, html) {
-        var nombre = "Rubén";
-        var html = html.toString();
-        
+        var nombre = "Ruben";
+        var html_string = html.toString();
+        // Expresion regular que busca en index.html los simbolos { } y devuelve un array con los valores que contienen
+        var variables = html_string.match(/[^\{\}]+(?=\})/g);
+        //variables = ['nombre']
+        for (var i = 0; i < variables.length; i++) {
+            var value = eval(variables[i]);
+            html_string =  html_string.replace("{"+variables[i]+"}", value);
+        }
         res.writeHead(200,{"Content-Type":"text/html"});
-        res.write(html);
+        res.write(html_string);
         console.log("peticion recibida");
-      
     });
 
 
